c983ca41492ba610999e8a0053466b18440793b0
castiel <castiel.andrew@gmail.com>
2018-02-16 01:12:18 -0800

    prep to sync branch

---
+++ NWMuseumAR/Controllers/Progress/ProgressCell.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressCell.swift
+//  NWMuseumAR
+//
+//  Created by Castiel Li on 2018-02-16.
+//  Copyright © 2018 NWMuseumAR. All rights reserved.
+//
+
+import UIKit
+
+class ProgressCell: UITableViewCell {
+    @IBOutlet weak var artifactIcon: UIImageView!
+    @IBOutlet weak var artifactDescription: UILabel!
+    
+    //Set the image view and label to display artifact information
+    func setArtifact(artifact: Artifact) {
+        artifactIcon.image = artifact.icon
+        artifactDescription.text = artifact.desc
+    }
+}
+
--- NWMuseumAR/Controllers/Progress/ProgressViewController.swift
+++ NWMuseumAR/Controllers/Progress/ProgressViewController.swift
@@ -10,26 +10,63 @@ import UIKit
 
 class ProgressViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var artifacts: [Artifact] = []
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
+        
+        artifacts = createArtifacts()
+        
+        //assigned tableview to the controller
+        //in order to use the extension methods
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //creates an array of artifacts as dummy data for testing
+    //This will be replaced by database of some sort later on
+    func createArtifacts() -> [Artifact]
+    {
+        var tempArtifacts: [Artifact] = []
+        
+        let Artifact1 = Artifact(icon: #imageLiteral(resourceName: "icon1"), desc: "Pick up that skull")
+        let Artifact2 = Artifact(icon: #imageLiteral(resourceName: "icon2"), desc: "Lighting in that box")
+        let Artifact3 = Artifact(icon: #imageLiteral(resourceName: "icon3"), desc: "Need a flower")
+        let Artifact4 = Artifact(icon: #imageLiteral(resourceName: "icon4"), desc: "Protect Binary")
+        let Artifact5 = Artifact(icon: #imageLiteral(resourceName: "icon5"), desc: "No idea whats this")
+        let Artifact6 = Artifact(icon: #imageLiteral(resourceName: "icon6"), desc: "Grow up")
+        
+        tempArtifacts.append(Artifact1)
+        tempArtifacts.append(Artifact2)
+        tempArtifacts.append(Artifact3)
+        tempArtifacts.append(Artifact4)
+        tempArtifacts.append(Artifact5)
+        tempArtifacts.append(Artifact6)
+        
+        return tempArtifacts
+    }
+}
+
+//find the cell and display info as needed
+extension ProgressViewController: UITableViewDataSource, UITableViewDelegate {
+    //this function sets how mant rolls are we displaying
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //display all the artifacts in the array
+        return artifacts.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //select an artifact based on the row number
+        let artifact = artifacts[indexPath.row]
+        //create an instance of the cell on that roll
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArtifactCell") as! ProgressCell
+        //call setArtifact function in ProgressCell.swift
+        cell.setArtifact(artifact: artifact)
+        //return the cell after update with icon and description
+        return cell
     }
-    */
-
 }
