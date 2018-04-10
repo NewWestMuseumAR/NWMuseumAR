d12617607f68f996d89aea65fc0b28bfbfe33f40
HMilbradt <harrison.milbradt@gmail.com>
2018-04-10 10:33:27 -0700

    Refactoring delegate, adding checks for ARKIT and iOS 11.3 (required for static image recognition)

--- NWMuseumAR/Controllers/AppDelegate.swift
+++ NWMuseumAR/Controllers/AppDelegate.swift
@@ -8,37 +8,60 @@
 
 import UIKit
 import CoreData
+import ARKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "NWMuseumAR")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Should be replaced with code to fail gracefully
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        
+    
         let homeViewController: UIViewController
         
-        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
-        
-        if launchedBefore  {
-            // Skip Tutorial
-            homeViewController = MainPageViewController()
+        // iOS 11.3 required, ARWorldTracking capable chip required
+        if ARWorldTrackingConfiguration.isSupported, #available(iOS 11.3, *){
+            
+            let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+            
+            if launchedBefore  {
+                // Skip Tutorial
+                homeViewController = MainPageViewController()
+            } else {
+                // Show tutorial, add launchedbefore to storage
+                homeViewController = TutorialPageViewController.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+                //UserDefaults.standard.set(true, forKey: "launchedBefore")
+            }
+            
+            // seed database if not already seeded
+            let seeded = UserDefaults.standard.bool(forKey: "seeded")
+            debugPrint("Seeded? \(seeded)")
+            if !seeded {
+                debugPrint("Seeding database")
+                seedDatabase()
+                UserDefaults.standard.set(true, forKey: "seeded")
+            }
+            
         } else {
-            // Show tutorial, add launchedbefore to storage
-            homeViewController = TutorialPageViewController.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-            //UserDefaults.standard.set(true, forKey: "launchedBefore")
+            // TODO: - Set this to unsupported device controller
+            homeViewController = TutorialPageViewController()
         }
-        
-        let seeded = UserDefaults.standard.bool(forKey: "seeded")
-        debugPrint("Seeded? \(seeded)")
-        if !seeded {
-            debugPrint("Seeding database")
-            seedDatabase()
-            UserDefaults.standard.set(true, forKey: "seeded")
-        }
-        
+
         // Show our starting controller to the user
         window!.rootViewController = homeViewController
         window!.makeKeyAndVisible()
@@ -83,56 +106,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         debugPrint("database seeded")
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         self.saveContext()
     }
-    
-    // MARK: - Core Data stack
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
-        let container = NSPersistentContainer(name: "NWMuseumAR")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
     
     // MARK: - Core Data Saving support
     func saveContext () {
